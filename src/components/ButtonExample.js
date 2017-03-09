6bde22a799084b3339859f1a8a7ced87c5334d85
Ian Rose <ianrosedev@gmail.com>
2017-03-09 01:12:46 -0500

    Initial Commit

---
+++ src/components/ButtonExample.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const ButtonExample = () => (
+  <div>
+    <Button basic color='red'>Hello,</Button>
+    <Button basic color='blue'>World!</Button>
+  </div>
+);
+
+export default ButtonExample;
---
+++ src/containers/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ButtonExample from '../components/ButtonExample';
+
+const App = () => (
+  <ButtonExample />
+);
+
+export default App;
