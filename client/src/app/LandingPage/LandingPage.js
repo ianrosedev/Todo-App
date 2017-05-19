44e8991d06ee5d10fcb2af7a7ba0a1fa254d32aa
Ian Rose <ianrosedev@gmail.com>
2017-05-19 01:17:23 -0400

    Fixed issue for MainContent: Dropdowns at the bottom were cutoff, Working on LandingPage style

--- client/src/app/LandingPage/LandingPage.js
+++ client/src/app/LandingPage/LandingPage.js
@@ -16,11 +16,14 @@ class LandingPage extends Component {
               <Icon name='list' size='huge' />
               <h1>Todo List</h1>
             </span>
-            <span id='quote'><em>It's easy to keep track of the things you need to get done!</em></span>
+            <span id='quote'>
+              <em>It's easy to keep track of the things you need to get done!</em>
+            </span>
             <Button
               as={Link}
               to='/users'
               color='green'
+              size='large'
             >
               Login
             </Button>
